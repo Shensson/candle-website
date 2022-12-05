c5c4d506e2f641284e7e185142b150471390bdea
shensson <daniali13ion@gmail.com>
2022-12-05 02:05:50 +0200

    search bar added

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ function myFunction(){
     navbar.classList.remove("sticky");
   }
 }
+
+let searchBtn = document.querySelector('.searchBtn');
+let closeBtn = document.querySelector('.closeBtn');
+let searchBox = document.querySelector('.searchBox');
+
+searchBtn.onclick = function(){
+  searchBox.classList.add('active');
+  closeBtn.classList.add('active');
+  searchBtn.classList.add('active');
+}
+closeBtn.onclick = function(){
+  searchBox.classList.remove('active');
+  closeBtn.classList.remove('active');
+  searchBtn.classList.remove('active');
+}
+
