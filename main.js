70788adbb8f800a70da02f0d7824d340520513c3
shensson <daniali13ion@gmail.com>
2022-12-26 03:55:45 +0200

    redoing everything with new knowledge

--- main.js
+++ main.js
@@ -1,29 +1,2 @@
-/* sticky header */
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky =navbar.offsetTop;
-
-function myFunction(){
-  if (window.pageYOffset >= sticky){
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-/* search box buttons */
-let searchBtn = document.querySelector('.searchBtn');
-let closeBtn = document.querySelector('.closeBtn');
-let searchBox = document.querySelector('.searchBox');
-
-searchBtn.onclick = function(){
-  searchBox.classList.add('active');
-  closeBtn.classList.add('active');
-  searchBtn.classList.add('active');
-}
-closeBtn.onclick = function(){
-  searchBox.classList.remove('active');
-  closeBtn.classList.remove('active');
-  searchBtn.classList.remove('active');
-}
 
